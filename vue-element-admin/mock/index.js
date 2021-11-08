ded0c00189062d3910f55b555a0e7104e0c4afd3
lguo <lguo@aibee.com>
2021-11-08 10:37:08 +0800

    submit test

--- vue-element-admin/mock/index.js
+++ vue-element-admin/mock/index.js
@@ -5,12 +5,15 @@ const user = require('./user')
 const role = require('./role')
 const article = require('./article')
 const search = require('./remote-search')
+const test = require('./test')
+
 
 const mocks = [
   ...user,
   ...role,
   ...article,
-  ...search
+  ...search,
+  ...test
 ]
 
 // for front mock
